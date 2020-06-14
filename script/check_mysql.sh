eacf19bc3a600fb47e7244646658c5378f5a9a11
xiaobitao <xiaobitao@wimetro.com>
2020-06-14 15:59:48 +0800

    add mysql inistall

---
+++ script/check_mysql.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+mysql --host=localhost --user=wim --password=wim123 -Nse "select 1 from dual"
---
+++ script/hadoop_env.sh
@@ -0,0 +1,10 @@
+export HADOOP_HOME=/home/hadoop/hadoop-3.1.2
+export HADOOP_CONF=$HADOOP_HOME/etc/hadoop
+export PATH=$HADOOP_HOME/sbin:$HADOOP_HOME/bin:$PATH
+export HADOOP_LOG_DIR=/home/hadoop/hadoop_logs
+export YARN_LOG_DIR=$HADOOP_LOG_DIR
+export HDFS_NAMENODE_USER="root"
+export HDFS_DATANODE_USER="root"
+export HDFS_SECONDARYNAMENODE_USER="root"
+export YARN_RESOURCEMANAGER_USER="root"
+export YARN_NODEMANAGER_USER="root"
